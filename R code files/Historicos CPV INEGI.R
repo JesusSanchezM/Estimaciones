f9f494c311d997398fde1e36fd9247dad1403b7b
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2025-05-23 11:59:07 -0600

    .

--- R code files/Historicos CPV INEGI.R
+++ R code files/Historicos CPV INEGI.R
@@ -13,8 +13,8 @@ library(tidyverse)
 #-----------------------------------
 
 {
-cpv_2020 <- read_csv("C:/Users/Jesus Sanchez/Desktop/ALEXIS/1. Estudio/Maestría BUAP - Economía/0. TESIS/Datos/Historico CPV INEGI/cpv_2020_personas/Censo2020_CPV_CB_Personas_ejemplo_csv.CSV")
-cpv_2010 <- read_dta("C:/Users/Jesus Sanchez/Desktop/ALEXIS/1. Estudio/Maestría BUAP - Economía/0. TESIS/Datos/Historico CPV INEGI/cpv_2010_personas/DUMMY_PERSONAS.dta")
+cpv_2020 <- read_csv("C:/Users/Jesus Sanchez/Desktop/ALEXIS/1. Estudio/Maestria BUAP - Economia/0. TESIS/Datos/Historico CPV INEGI/cpv_2020_personas/Censo2020_CPV_CB_Personas_ejemplo_csv.CSV")
+cpv_2010 <- read_dta("C:/Users/Jesus Sanchez/Desktop/ALEXIS/1. Estudio/Maestria BUAP - Economia/0. TESIS/Datos/Historico CPV INEGI/cpv_2010_personas/DUMMY_PERSONAS.dta")
 
 
 
