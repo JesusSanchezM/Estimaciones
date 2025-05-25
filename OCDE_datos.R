431b612280e32b4254c95d926714997987d02e34
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2025-05-25 11:52:28 -0600

    ampliacion de datos de 2005 a 2023

--- OCDE_datos.R
+++ OCDE_datos.R
@@ -2,25 +2,57 @@ library(tidyverse)
  
 # Crear datos largos con error estándar
 datos <- tibble::tibble(
-  país   = rep(c("México", "OCDE"), each = 18),
-  año    = rep(rep(2018:2023, each = 3), times = 2),
-  nivel  = rep(c("Below upper secondary education", "Upper secondary or post-secondary non-tertiary education", "Tertiary education"), times = 6 * 2),
-  valor  = c(
+  país = rep(c("México", "OCDE"), each = 3 * 19),
+  año = rep(rep(2005:2023, each = 3), times = 2),
+  nivel = rep(c(
+    "Below upper secondary education",
+    "Upper secondary or post-secondary non-tertiary education",
+    "Tertiary education"
+  ), times = 19 * 2),
+  valor = c(
     # México
-    60.85, 21.17, 17.98, 60.21, 21.53, 18.26, 58.40, 22.16, 19.43,
-    57.19, 22.35, 20.46, 56.22, 23.15, 20.64, 55.56, 23.48, 20.96,
+    71.8, 15.4, 12.7,
+    71.0, 15.8, 13.3,
+    70.4, 16.0, 13.6,
+    70.1, 16.2, 13.7,
+    68.7, 17.0, 14.3,
+    67.9, 17.4, 14.7,
+    66.9, 18.1, 15.0,
+    66.0, 18.3, 15.7,
+    65.2, 18.6, 16.2,
+    64.9, 19.1, 16.0,
+    64.3, 19.4, 16.3,
+    63.4, 19.8, 16.8,
+    62.3, 20.2, 17.4,
+    60.9, 21.2, 18.0,
+    60.2, 21.5, 18.3,
+    58.4, 22.2, 19.4,
+    57.2, 22.4, 20.5,
+    56.2, 23.1, 20.6,
+    55.6, 23.5, 21.0,
     # OCDE
-    22.02, 41.51, 36.89, 21.42, 41.07, 37.92, 19.81, 41.40, 39.21,
-    19.91, 40.53, 39.99, 19.52, 40.22, 40.68, 19.23, 40.23, 40.95
-  ),
-  error_std = c(
-    # México
-    0.11, 0.09, 0.09, 0.11, 0.09, 0.09, 0.12, 0.10, 0.09,
-    0.11, 0.09, 0.09, 0.11, 0.09, 0.09, 0.11, 0.09, 0.09,
-    # OCDE: sin datos de error estándar
-    rep(NA, 18)
+    30.0, 44.2, 26.3,
+    29.3, 44.7, 26.5,
+    28.5, 44.2, 27.6,
+    27.7, 44.1, 28.6,
+    27.1, 43.9, 29.3,
+    27.0, 43.5, 29.9,
+    26.8, 43.3, 30.4,
+    25.5, 43.2, 31.8,
+    25.0, 43.1, 32.3,
+    24.3, 42.4, 33.6,
+    24.2, 42.2, 34.0,
+    23.0, 42.2, 35.3,
+    22.8, 41.8, 35.8,
+    22.0, 41.5, 36.9,
+    21.4, 41.1, 37.9,
+    19.8, 41.4, 39.2,
+    19.9, 40.5, 40.0,
+    19.5, 40.2, 40.7,
+    19.2, 40.2, 41.0
   )
 )
+
 
 datos <- datos |>
   mutate(
@@ -65,7 +97,7 @@ datos_pos <- datos |>
   arrange(país, año, desc(nivel)) |>
   mutate(
     pos = cumsum(valor) - valor / 2,
-    etiqueta = sprintf("%.2f%%", valor)
+    etiqueta = sprintf("%.1f%%", valor)
   )
 
 
@@ -73,7 +105,7 @@ datos_pos |>
   filter(país == "México") |>
   ggplot(aes(x = factor(año), y = valor, fill = nivel)) +
   geom_bar(stat = "identity", color="black", size=1) +
-  geom_text(aes(y = pos, label = etiqueta), color = "white", size = 4) +
+  geom_text(aes(y = pos, label = etiqueta), color = "white", size = 2.5) +
   labs(
     title = "Distribución apilada del nivel educativo en México (2018–2023)",
     x = "Año", y = "Porcentaje total", fill = "Nivel educativo"
@@ -85,7 +117,7 @@ datos_pos |>
   filter(país == "OCDE") |>
   ggplot(aes(x = factor(año), y = valor, fill = nivel)) +
   geom_bar(stat = "identity", , color="black", size=1) +
-  geom_text(aes(y = pos, label = etiqueta), color = "white", size = 4) +
+  geom_text(aes(y = pos, label = etiqueta), color = "white", size = 2.5) +
   labs(
     title = "Distribución apilada del nivel educativo en países OCDE (2018–2023)",
     x = "Año", y = "Porcentaje total", fill = "Nivel educativo"
@@ -94,8 +126,7 @@ datos_pos |>
 
 
 ggplot(datos_pos, aes(x = factor(año), y = valor, fill = nivel)) +
-  geom_bar(stat = "identity", color="black", size=1) +
-  geom_text(aes(y = pos, label = etiqueta), color = "white", size = 3.5) +
+  geom_bar(stat = "identity", color="black", size=1) + 
   facet_wrap(~ país) +
   labs(
     title = "Distribución educativa apilada: México vs OCDE (2018–2023)",
